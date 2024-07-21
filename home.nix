bbb0e48841135a0534c5848cb17dc1c630b4d949
Max Afanasev <max@afsv.me>
2024-07-21 16:02:53 +0200

    add qbittorrent

--- home.nix
+++ home.nix
@@ -21,9 +21,7 @@
     wl-clipboard
     nerdfonts
     telegram-desktop
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    qbittorrent
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
